d040e4b6d4281c35fa8906b945f80532a1da9c72
Unknown <valerauko@gmail.com>
2018-02-18 20:06:48 +0900

    Add favicon

--- src/books/view.clj
+++ src/books/view.clj
@@ -14,12 +14,11 @@
         [:meta {:http-equiv "Content-Type" :content "text/html; charset=utf-8"}]
         [:meta {:name "viewport" :content "width=device-width, initial-scale=1, shrink-to-fit=no"}]
         [:title title]
-        [:link {:type "text/css" :rel "stylesheet"
+        [:link {:type "image/png" :rel "icon" :href "favicon.png"}]
+        [:link {:type "text/css" :rel "stylesheet" :crossorigin "anonymous"
                 :href "https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
-                :integrity "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
-                :crossorigin "anonymous"}]
-        [:link {:type "text/css" :rel "stylesheet"
-                :href "books.css"}]]
+                :integrity "sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"}]
+        [:link {:type "text/css" :rel "stylesheet" :href "books.css"}]]
       [:body {:class "text-center"}
         [:h1 title]
         body])))
