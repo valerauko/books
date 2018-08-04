d7d25b4a6df24ef669ae4bf50e8bab4484e642d3
Unknown <vale@valerauko.net>
2018-08-04 12:42:37 +0900

    Add lein ancient and upgrade

--- project.clj
+++ project.clj
@@ -5,12 +5,13 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [hiccup "1.0.5"]
-                 [congomongo "0.5.1"]
-                 [compojure "1.6.0"]
+                 [congomongo "0.5.2"]
+                 [compojure "1.6.1"]
                  [buddy/buddy-hashers "1.3.0"]
-                 [ring-logger "0.7.7"]
-                 [ring/ring-defaults "0.3.1"]]
+                 [ring-logger "1.0.1"]
+                 [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler books.handler/app}
   :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[ring/ring-mock "0.3.2"]]}})
+             :dev {:dependencies [[ring/ring-mock "0.3.2"]]
+                   :plugins [[lein-ancient "0.6.15"]]}})
