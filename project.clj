7c8b3525e30c19d928d9fd6f88ae19b4fb36cb2a
Unknown <vale@valerauko.net>
2018-08-04 13:01:23 +0900

    Bump version and change jar name

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject books "0.1.3"
+(defproject books "0.1.4"
   :description "Clojure app to track what you read"
   :url "https://books.valerauko.net"
   :license {:name "Eclipse Public License"
@@ -12,6 +12,7 @@
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler books.handler/app}
-  :profiles {:uberjar {:aot :all}
+  :profiles {:uberjar {:aot :all
+                       :uberjar-name "books.jar"}
              :dev {:dependencies [[ring/ring-mock "0.3.2"]]
                    :plugins [[lein-ancient "0.6.15"]]}})
