cbc71f47bfa3e452e8b8152702fa1db9e8f99261
Vale <valerauko@gmail.com>
2019-05-31 03:03:27 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,14 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [hiccup "1.0.5"]
-                 [congomongo "0.5.2"]
+                 [congomongo "1.1.0"]
                  [compojure "1.6.1"]
                  [buddy/buddy-hashers "1.3.0"]
                  [ring-logger "1.0.1"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.9.7"]]
+  :plugins [[lein-ring "0.12.5"]]
   :ring {:handler books.handler/app}
   :profiles {:uberjar {:aot :all
                        :uberjar-name "books.jar"}
-             :dev {:dependencies [[ring/ring-mock "0.3.2"]]
+             :dev {:dependencies [[ring/ring-mock "0.4.0"]]
                    :plugins [[lein-ancient "0.6.15"]]}})
