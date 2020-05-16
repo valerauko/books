87672d31baba4584c049b09aef82f1ef6f29612a
Vale <valerauko@gmail.com>
2020-05-16 18:50:13 +0900

    Update dependencies

--- project.clj
+++ project.clj
@@ -5,16 +5,16 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [hiccup "1.0.5"]
-                 [congomongo "1.1.0"]
+                 [congomongo "2.2.1"]
                  [compojure "1.6.1"]
                  [mount "0.1.16"]
-                 [buddy/buddy-hashers "1.3.0"]
+                 [buddy/buddy-hashers "1.4.0"]
                  [ring-logger "1.0.1"]
-                 [ring/ring-core "1.7.1"]
-                 [ring/ring-jetty-adapter "1.7.1"]
+                 [ring/ring-core "1.8.1"]
+                 [ring/ring-jetty-adapter "1.8.1"]
                  [ring/ring-defaults "0.3.2"]]
   :main ^:skip-aot books.handler
-  :jvm-opts ["-Xmx600m" "-server"]
+  :jvm-opts ["-Xmx100m" "-server"]
   :jar-name "books-%s-slim.jar"
   :profiles {:uberjar {:uberjar-name "books-%s.jar"
                        :aot :all}
